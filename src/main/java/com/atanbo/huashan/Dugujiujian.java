7e412a4f678c5b19cf0695f1de61c5e9c7c8e3de
ab5224153 <2391826033@qq.com>
2020-07-10 18:31:45 +0800

    dugujiujian

---
+++ src/main/java/com/atanbo/huashan/Dugujiujian.java
@@ -0,0 +1,19 @@
+package com.atanbo.huashan;
+
+/**
+ * @author anbo
+ * @create 2020-07-10 15:52
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+
+
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+
+
+
+    }
+}
